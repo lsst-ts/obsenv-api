1cffa2bff36f635701d8361503842bcbbef3785e
Michael Reuter <mareuter@lsst.org>
2024-05-09 16:29:46 -0700

    Add dependencies and formatting.

--- src/obsenvapi/config.py
+++ src/obsenvapi/config.py
@@ -14,9 +14,7 @@ class Config(BaseSettings):
 
     name: str = Field("obsenv-api", title="Name of application")
 
-    path_prefix: str = Field(
-        "/obsenv-api", title="URL prefix for application"
-    )
+    path_prefix: str = Field("/obsenv-api", title="URL prefix for application")
 
     profile: Profile = Field(
         Profile.development, title="Application logging profile"
